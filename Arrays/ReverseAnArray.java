d8c9f1f0d3184ae67a66fd9cfa758084609eb50b
Bheb Developer <bhebdeveloper@gmail.com>
2024-07-04 00:16:32 +0530

    04/07/2024

--- Arrays/ReverseAnArray.java
+++ Arrays/ReverseAnArray.java
@@ -13,8 +13,8 @@ public class ReverseAnArray {
     static void reverseArray(int[] arr) {
         int size = arr.length;
 
-        for (int i = 0; i < size / 2; i++) {
-            int temp = arr[size - 1 - i];
+        for (int i = 0; i < size / 2; i++) { // we need half of the size (sec half is replaced by first half)
+            int temp = arr[size - 1 - i]; // size - 1 is last item
             arr[size - 1 - i] = arr[i];
             arr[i] = temp;
         }
