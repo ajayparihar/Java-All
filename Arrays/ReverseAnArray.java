af8946d4212e562e1759e8d538610d4bef6e8ea8
Bheb Developer <bhebdeveloper@gmail.com>
2024-07-04 03:12:43 +0530

    04/07/2024

--- Arrays/ReverseAnArray.java
+++ Arrays/ReverseAnArray.java
@@ -4,36 +4,39 @@ import java.util.*;
 
 public class ReverseAnArray {
     public static void main(String[] args) {
-        int[] arr = { 1, 2, 3, 4, 5, 6 };
+        int[] nums = { 1, 2, 3, 4, 5, 6 };
         int start = 0;
-        int end = 2;
-        revArrayCollection(arr);
-        reverseArray(arr, start, end);
+        int end = 5;
+        revnumsayCollection(nums);
+        reversenumsay(nums, start, end);
 
-        for (int i : arr) {
+        for (int i : nums) {
             System.out.print(i + " ");
         }
     }
 
-    static void reverseArray(int[] arr, int start, int end) {
+    static void reversenumsay(int[] nums, int start, int end) {
 
-        for (int i = start; i < end - 1; i++) {
-            int temp = arr[end - i];
-            arr[end - i] = arr[i];
-            arr[i] = temp;
+        while (start < end) {
+            int temp = nums[end];
+            nums[end] = nums[start];
+            nums[start] = temp;
+
+            start++;
+            end--;
         }
     }
 
-    static void revArrayCollection(int[] arr) {
+    static void revnumsayCollection(int[] nums) {
 
-        List<Integer> arrList = new ArrayList<>();
+        List<Integer> numsList = new ArrayList<>();
 
-        for (int i : arr) {
-            arrList.add(i);
+        for (int i : nums) {
+            numsList.add(i);
         }
-        Collections.reverse(arrList);
+        Collections.reverse(numsList);
         System.out.println(" Collections.Reverse");
-        for (int i : arrList) {
+        for (int i : numsList) {
             System.out.print(i + " ");
         }
         System.out.println("");
