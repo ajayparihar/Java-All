6130ab811a8d43d7bf118fa8d8062c69094667a5
Bheb Developer <bhebdeveloper@gmail.com>
2024-07-04 03:33:50 +0530

    04/07/2024

---
+++ Arrays/RotateAnArray.java
@@ -0,0 +1,35 @@
+package Arrays;
+
+public class RotateAnArray {
+    public static void main(String[] args) {
+        int[] nums = { 1, 2, 3, 4, 5, 6 };
+        int k = 3;
+        rotateArray(nums, k);
+
+        for (int i : nums) {
+            System.out.print(i + " ");
+        }
+    }
+
+    static void rotateArray(int[] nums, int k) {
+        int n = nums.length;
+        k = k % n;
+        if (k == 0)
+            return;
+        reverse(nums, 0, n - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, n - 1);
+    }
+
+    static void reverse(int[] nums, int start, int end) {
+
+        while (start < end) {
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+
+            start++;
+            end--;
+        }
+    }
+}
