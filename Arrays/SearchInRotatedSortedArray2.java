ae98c3657e27a8fdd5963967ee58f0cd0c246b6e
Bheb Developer <bhebdeveloper@gmail.com>
2024-07-18 23:38:37 +0530

    18/07/2024

--- Arrays/SearchInRotatedSortedArray2.java
+++ Arrays/SearchInRotatedSortedArray2.java
@@ -1,33 +1,38 @@
-import java.util.*;
+package Arrays;
 
-public class Main {
-    public static void main(String[] args) {
-     int[] nums = {1,0,1,1,1};
-     int target = 0;
-     
-     System.out.print(search(nums, target));
+public class SearchInRotatedSortedArray2 {
+  public static void main(String[] args) {
+    int[] nums = { 1, 0, 1, 1, 1 };
+    int target = 0;
+
+    System.out.print(search(nums, target));
   }
-  
-  static boolean search(int[] nums, int target){
+
+  static boolean search(int[] nums, int target) {
     int left = 0;
     int right = nums.length - 1;
-    
-    while(left <= right){
+
+    while (left <= right) {
       int mid = left + (right - left) / 2;
-      
-      if(nums[mid] == target) return true;
-      
-      if(nums[left] == nums[mid]){
+
+      if (nums[mid] == target)
+        return true;
+
+      if (nums[left] == nums[mid]) {
         left++;
         continue;
       }
-      
-      if(nums[left] <= nums[mid]){
-        if(nums[left] <= target && target <= nums[mid]) right = mid - 1;
-        else left = mid + 1;
-      }else{
-        if(nums[mid] < target && target <= nums[right]) left = mid + 1;
-        else right = mid - 1;
+
+      if (nums[left] <= nums[mid]) {
+        if (nums[left] <= target && target <= nums[mid])
+          right = mid - 1;
+        else
+          left = mid + 1;
+      } else {
+        if (nums[mid] < target && target <= nums[right])
+          left = mid + 1;
+        else
+          right = mid - 1;
       }
     }
     return false;
