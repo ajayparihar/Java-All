9e19ba0d9ffa77d34615d7403da2782815643235
Bheb Developer <bhebdeveloper@gmail.com>
2024-08-17 00:29:52 +0530

    17/08/2024

---
+++ Arrays/LemonadeChange.java
@@ -0,0 +1,14 @@
+package Arrays;
+
+import java.util.*;
+
+public class LemonadeChange {
+    public static void main(String[] args) {
+        int[] bills = { 5, 5, 5, 10, 20 };
+      return lemonadeChange(bills);
+    }
+
+    static boolean lemonadeChange(int[] bills) {
+        return true;
+    }
+}
