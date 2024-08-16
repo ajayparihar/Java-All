321f427bae9f38c500672adfee5d046712644972
Bheb Developer <bhebdeveloper@gmail.com>
2024-08-17 00:41:32 +0530

    17/08/2024

--- Arrays/LemonadeChange.java
+++ Arrays/LemonadeChange.java
@@ -9,6 +9,29 @@ public class LemonadeChange {
     }
 
     static boolean lemonadeChange(int[] bills) {
+      int countOfFive = 0;
+      int countOfTen = 0;
+      
+      for(int bill : bills){
+        if(bill == 5){
+          countOfFive++;
+        }else if(bill == 10){
+          if(countOfFive == 0){
+            return false;
+          }
+          countOfFive--;
+          countOfTen++;
+        } else{
+          if(countOfTen > 0 && countOfFive > 0){
+            countOfTen--;
+            countOfFive--;
+          } else if(countOfFive >= 3){
+            countOfFive -=3;
+          }else {
+            return false;
+          }
+        }
+      }
         return true;
     }
 }
