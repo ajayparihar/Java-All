a7fa4f21283e024b2b8a4d8ab01f2d7a45c1bdfb
Bheb Developer <bhebdeveloper@gmail.com>
2024-09-17 14:44:39 +0530

    17/09/2024

--- Arrays/LemonadeChange.java
+++ Arrays/LemonadeChange.java
@@ -1,37 +1,37 @@
 package Arrays;
 
-import java.util.*;
-
 public class LemonadeChange {
-    public static void main(String[] args) {
-        int[] bills = { 5, 5, 5, 10, 20 };
-      return lemonadeChange(bills);
-    }
+  public static void main(String[] args) {
+    int[] bills = { 5, 5, 5, 10, 20 };
+    boolean result = lemonadeChange(bills);
 
-    static boolean lemonadeChange(int[] bills) {
-      int countOfFive = 0;
-      int countOfTen = 0;
-      
-      for(int bill : bills){
-        if(bill == 5){
-          countOfFive++;
-        }else if(bill == 10){
-          if(countOfFive == 0){
-            return false;
-          }
+    System.out.println(result);
+  }
+
+  static boolean lemonadeChange(int[] bills) {
+    int countOfFive = 0;
+    int countOfTen = 0;
+
+    for (int bill : bills) {
+      if (bill == 5) {
+        countOfFive++;
+      } else if (bill == 10) {
+        if (countOfFive == 0) {
+          return false;
+        }
+        countOfFive--;
+        countOfTen++;
+      } else {
+        if (countOfTen > 0 && countOfFive > 0) {
+          countOfTen--;
           countOfFive--;
-          countOfTen++;
-        } else{
-          if(countOfTen > 0 && countOfFive > 0){
-            countOfTen--;
-            countOfFive--;
-          } else if(countOfFive >= 3){
-            countOfFive -=3;
-          }else {
-            return false;
-          }
+        } else if (countOfFive >= 3) {
+          countOfFive -= 3;
+        } else {
+          return false;
         }
       }
-        return true;
     }
+    return true;
+  }
 }
