316d68acd668ad25f35fe19f9a0f6b378f4fdb45
Bheb Developer <bhebdeveloper@gmail.com>
2025-02-13 04:17:36 +0530

    Add time and space complexity comments to MaxPairSumWithEqualDigits_BruteForce class

--- Arrays/MaxPairSumWithEqualDigits_BruteForce.java
+++ Arrays/MaxPairSumWithEqualDigits_BruteForce.java
@@ -1,40 +1,48 @@
 package Arrays;
 
+// Time Complexity: O(n^2)
+// Space Complexity: O(n)
 public class MaxPairSumWithEqualDigits_BruteForce {
-public static void main(String[] args) {
-    int[] nums = {18,43,36,13,7};
 
-    System.out.println(maximumSum(nums));
-}
+    public static void main(String[] args) {
+        int[] nums = {18, 43, 36, 13, 7};
 
-static int maximumSum(int[] nums){
-    int[] sumOfNum = new int[nums.length];
+        System.out.println(maximumSum(nums));
+    }
+
+    static int maximumSum(int[] nums) {
+        int[] sumOfNum = new int[nums.length];
         int index = 0;
-        for(int i : nums){
+        // Calculate the sum of digits for each number in the array
+        for (int i : nums) {
             sumOfNum[index] = sumOfDigits(i);
             index++;
         }
         int maxSum = 0;
         int counter = 0;
-        for(int i = 0; i < nums.length; i++){
-            for(int j = i + 1; j < nums.length; j++){
-                if(sumOfNum[i] == sumOfNum[j]){
+        // Compare each pair of numbers to find the maximum sum of pairs with equal digit sums
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (sumOfNum[i] == sumOfNum[j]) {
                     counter++;
                     maxSum = Math.max(maxSum, nums[i] + nums[j]);
                 }
             }
         }
-        if(counter == 0){
+        // If no pairs found, return -1
+        if (counter == 0) {
             return -1;
         }
         return maxSum;
     }
-static int sumOfDigits(int num){
-      int sum = 0;
-      while(num > 0) {
-        sum += num % 10;
-        num /= 10;
-      } 
-      return sum;
-}
+
+    // Helper method to calculate the sum of digits of a number
+    static int sumOfDigits(int num) {
+        int sum = 0;
+        while (num > 0) {
+            sum += num % 10;
+            num /= 10;
+        }
+        return sum;
+    }
 }
